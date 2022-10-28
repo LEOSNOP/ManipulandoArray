7da0ca2602dd6827cae6aabed4fba37a66560c88
_L_ <conceicao.leonardo9@gmail.com>
2022-10-28 11:22:29 -0300

    terceiroFormasdeDeclararArrays

--- src/MeuArray/Exec.java
+++ src/MeuArray/Exec.java
@@ -2,8 +2,31 @@ package MeuArray;
 
 public class Exec {
     public static void main(String[] args) {
-        //Array de uma dimensão unidimensional
-        int[] array_de_uma_dimensao = new int[5];
+       //declarando array de uma dimensao
+
+        int[] a = new int[4];
+        
+
+        int [] r = new int [44], k = new int[23];
+
+        int[] iniciavalor ={2, 44, 666, 7, 8, 9, 13, 432};
+
+        int[]arrayExemploNInteiro;
+        arrayExemploNInteiro = new int[5];
+        arrayExemploNInteiro [0] = 100;
+        arrayExemploNInteiro [1] = 85;
+        arrayExemploNInteiro [2] = 88;
+        arrayExemploNInteiro [3] = 93;
+        arrayExemploNInteiro [4] = 123;
+
+        System.out.println(arrayExemploNInteiro[2]);
+        System.out.println(arrayExemploNInteiro[4]);
+
+        System.out.println(iniciavalor[2]);
+
+
+
+
 
 
 
