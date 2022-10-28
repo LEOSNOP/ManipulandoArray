3084ec47fda3939c9f4ba9b734a0838f509757d5
_L_ <conceicao.leonardo9@gmail.com>
2022-10-28 11:57:21 -0300

    quintoManipulandoArray

--- src/MeuArray/Exec.java
+++ src/MeuArray/Exec.java
@@ -2,35 +2,21 @@ package MeuArray;
 
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collection;
+
 
 public class Exec {
     public static void main(String[] args) {
-        //list
-        List<Integer> lista = new ArrayList<>();
+        //Array list
 
-        lista.add(1);
-        lista.add(2);
-        lista.add(3);
-        lista.add(9);
-        lista.add(5);
+        Collection<String> nomes = new ArrayList<>();
 
-        System.out.println("todos elementos da lista"+ lista);
+        nomes.add("LEO");
+        nomes.add("Isa");
+        nomes.add("Lucas");
+        nomes.add("luiza");
 
-        int tamanho = lista.size();
-
-        System.out.println("tamanho total dos elementos = " + tamanho);
-
-
-
-
-
-
-
-
-
-
-
+        System.out.println("Nomes que estao na lista: " + nomes);
 
     }
 }
