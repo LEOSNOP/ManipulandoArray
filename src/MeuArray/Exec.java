6a98170c1cf9f3c91ee131be19a47b70054f51e5
_L_ <conceicao.leonardo9@gmail.com>
2022-10-28 11:05:51 -0300

    primeirin

---
+++ src/MeuArray/Exec.java
@@ -0,0 +1,12 @@
+package MeuArray;
+
+public class Exec {
+    public static void main(String[] args) {
+        // inicializando um array
+        String[] nomes = {"leo", "mike", "luiz", "Ronin"};
+        for(int i = 0; i <= 3; i ++){
+            System.out.println(nomes[i]);
+        }
+    }
+
+}
